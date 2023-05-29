59e4a426781e357a86c000fb9d18544f1e5ebd29
karthick <prog@ecomsense.in>
2023-05-29 12:13:05 +0530

    XTS Binary

--- omspy_brokers/XTBinary/MarketdataSocketExample.py
+++ omspy_brokers/XTBinary/MarketdataSocketExample.py
@@ -10,16 +10,17 @@ from OpenInterestEvent import OpenInterest
 from binary_reader import BinaryReader
 
 
-API_KEY = "15cb10473a729b424cf932"
-API_SECRET = "Deej832$Nc" 
+API_KEY = "d7314c2c926629da0bf985"
+API_SECRET = "Juxj672@fS"
 source = "WEBAPI"
+
 b = bytearray()
-xt = XTSConnect(API_KEY, API_SECRET, source,"","")
+xt = XTSConnect(API_KEY, API_SECRET, source, "", "")
 response = xt.marketdata_login()
 print("Login: ", response)
 
 set_marketDataToken = response['result']['token']
-set_muserID = response['result']['userID'] 
+set_muserID = response['result']['userID']
 soc = MDSocket_io(set_marketDataToken, set_muserID)
 
 
@@ -27,7 +28,7 @@ soc = MDSocket_io(set_marketDataToken, set_muserID)
 def on_connect():
     """Connect from the socket."""
     print('Market Data Socket connected successfully!')
-    Instruments = [ {'exchangeSegment':1, 'exchangeInstrumentID': 22}]
+    Instruments = [{'exchangeSegment': 1, 'exchangeInstrumentID': 22}]
     response = xt.send_subscription(Instruments, 1501)
 
 
@@ -35,70 +36,70 @@ def on_connect():
 def on_message(data):
     print('I received a message!')
 
+
 def on_xts_binary_packet(data):
     try:
-        a = bytearray(data)   
+        a = bytearray(data)
         offset = 0
         count = 0
         currentsize = 0
         isnextpacket = True
-        datalen=len(a)
-        print(datetime.now() ,'I received a on_xts_binary_packet  message!' + str(len(data)))
-        
+        datalen = len(a)
+        print(datetime.now(),
+              'I received a on_xts_binary_packet  message!' + str(len(data)))
+
         br = BinaryReader(data)
         isGzipCompressed = br.read_int8()
-        offset=offset+1
+        offset = offset+1
         packetSize = 0
         uncompressedPacketSize = 0
         nextdata = a
-        if(datalen > packetSize):
-            if (isGzipCompressed == 1): 
-                while (isnextpacket): 
+        if (datalen > packetSize):
+            if (isGzipCompressed == 1):
+                while (isnextpacket):
                     nextdata = a[offset:datalen]
                     br = BinaryReader(nextdata)
                     packetSize = br.read_int32()
-                    offset=offset+4
+                    offset = offset+4
                     uncompressedPacketSize = br.read_int32()
                     offset += 4
                     filteredByteArray = a[offset:(offset + packetSize)]
-                    inflate = pako_inflate_raw(filteredByteArray);
+                    inflate = pako_inflate_raw(filteredByteArray)
                     result = bytearray(inflate)
                     r = BinaryReader(result)
                     currentsize = packetSize + offset
-                    if (currentsize < len(a)): 
-                                isnextpacket = True
-                                offset = currentsize
-                    else: isnextpacket = False
+                    if (currentsize < len(a)):
+                        isnextpacket = True
+                        offset = currentsize
+                    else:
+                        isnextpacket = False
 
-                    if (len(result) > 9) :
+                    if (len(result) > 9):
                         messageCode = str(r.read_uint16())
 
-                        print("MessageCode",messageCode)
-                        if ("1501" in messageCode) :
+                        print("MessageCode", messageCode)
+                        if ("1501" in messageCode):
                             print("TOUCHLINE MESSAGE")
-                            Touchline.deserialize(r,count)
+                            Touchline.deserialize(r, count)
                         elif ("1502" in messageCode):
                             print("MARKETDEPTH EVENT")
-                            MarketDepthEvent.deserialize(r,count)
+                            MarketDepthEvent.deserialize(r, count)
                         elif ("1510" in messageCode):
                             print("OpenInterest EVENT")
-                            OpenInterest.deserialize(r,count)
+                            OpenInterest.deserialize(r, count)
         else:
             count = offset
-            while (isnextpacket and datalen.size > 1 ): 
+            while (isnextpacket and datalen.size > 1):
                 messageCode = str(r.read_uint16())
-                print("MessageCode",messageCode)
-                if ("1501" in messageCode) :
+                print("MessageCode", messageCode)
+                if ("1501" in messageCode):
                     print("TOUCHLINE MESSAGE UNCOMPRESSED")
                 elif ("1502" in messageCode):
                     print("MARKETDEPTH EVENT UNCOMPRESSED")
-                    MarketDepthEvent.deserialize(r,count)
+                    MarketDepthEvent.deserialize(r, count)
                 elif ("1510" in messageCode):
                     print("OpenInterest EVENT UNCOMPRESSED")
-                    OpenInterest.deserialize(r,count)
-
-
-
+                    OpenInterest.deserialize(r, count)
 
     except Exception as e:
         print(e)
@@ -120,7 +121,6 @@ def pako_inflate_raw(data):
     decompressed_data = decompress.decompress(data)
     decompressed_data += decompress.flush()
     return decompressed_data
-
 
 
 # Assign the callbacks.
