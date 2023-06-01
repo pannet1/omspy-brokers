54a8f8f386b0ca6ebfd2a073eb76dee5c96269ca
karthick <prog@ecomsense.in>
2023-06-01 23:44:28 +0530

    adjust exception path for pip

--- omspy_brokers/XTConnect/Connect.py
+++ omspy_brokers/XTConnect/Connect.py
@@ -11,7 +11,7 @@ import logging
 
 import requests
 from six.moves.urllib.parse import urljoin
-import XTException as ex
+import omspy_brokers.XTConnect.XTException as ex
 
 log = logging.getLogger(__name__)
 
