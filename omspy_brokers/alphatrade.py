b8c9c631da7bed254e03186aa1de584a1a484336
karthick <prog@ecomsense.in>
2023-06-22 00:27:36 +0530

    add alphatrade

---
+++ omspy_brokers/alphatrade.py
@@ -0,0 +1,56 @@
+from alphatrade import AlphaTrade
+from omspy.base import Broker, pre, post
+import pyotp
+from typing import List, Dict
+
+
+class Alphatrade(Broker):
+    """
+    Automated Trading class
+    """
+
+    def __init__(self, user_id, passwd, totp):
+        self.user_id = user_id
+        self.passwd = passwd
+        self.totp = totp
+        pin = f"{int(pyotp.TOTP(totp).now()):06d}"
+        self.broker = AlphaTrade(login_id=user_id, password=passwd, twofa=pin)
+        super(Alphatrade, self).__init__()
+
+    def authenticate(self) -> str:
+        """
+        Authenticate the user
+        """
+        try:
+            access_token = open('access_token.txt', 'r').read().rstrip()
+            self.access_token = access_token
+        except Exception as e:
+            print('Exception occurred :: {}'.format(e))
+            return False
+        else:
+            return True
+
+    @ pre
+    def order_place(self, **kwargs):
+        pass
+
+    def order_modify(self, **kwargs):
+        pass
+
+    def order_cancel(self, order_id):
+        pass
+
+    @ property
+    @ post
+    def orders(self) -> List[Dict]:
+        return self.alice.get_order_history("")
+
+    @ property
+    @ post
+    def positions(self):
+        pass
+
+    @ property
+    @ post
+    def trades(self):
+        pass
