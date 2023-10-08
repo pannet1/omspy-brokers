86890e9fe0afecf4ad992007fdee0ce30fe25236
karthick <prog@ecomsense.in>
2023-10-09 02:49:52 +0530

    remove selenium

--- omspy_brokers/bypass.py
+++ omspy_brokers/bypass.py
@@ -3,11 +3,6 @@ from kiteext.kiteext import KiteExt
 from typing import List, Dict
 import pyotp
 import traceback
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 import time
 import requests
 
@@ -68,73 +63,6 @@ class Bypass(Broker):
             return False
         else:
             return True
-
-    def _login(self):
-        try:
-            # s = Service(ChromeDriverManager().install())
-            options = Options()
-            # options.add_argument('--headless')
-            options.add_argument('--disable-gpu')
-            options.add_argument("--no-sandbox")
-            options.add_argument("--disable-dev-shm-usage")
-
-            driver = webdriver.Chrome(service=ChromeService(
-                ChromeDriverManager().install()), options=options)
-            # driver = webdriver.Chrome(service=s, options=options)
-            driver.get('https://kite.zerodha.com/')
-            driver.implicitly_wait(5)
-            time.sleep(5)
-
-            # Find User ID and Password input
-            username = driver.find_element(
-                By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/form/div[2]/input')
-            password = driver.find_element(
-                By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/form/div[3]/input')
-
-            # Type User ID and Passwod
-            username.send_keys(self.userid)
-            password.send_keys(self.password)
-
-            # click on Login
-            driver.find_element(
-                By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/form/div[4]/button').click()
-
-            time.sleep(5)
-            # Find input to enter Pin
-            pin = driver.find_element("xpath", "//input[@type='text']")
-
-            # Type the Pin
-            otp = pyotp.TOTP(self.totp).now()
-            twoFA = f"{int(otp):06d}" if len(otp) <= 5 else otp
-            pin.send_keys(twoFA)
-
-            # Click on Continue
-            """
-            driver.find_element(
-                By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div/div/form/div[3]/button').click()
-            """
-
-            # Store the Cookies and enctoken
-            time.sleep(3)
-            cookie = driver.get_cookies()
-
-            # Update Enctoken
-            for idx, each_dict in enumerate(cookie):
-                if each_dict['name'] == 'public_token':
-                    # 'kite' is added to Domain for login on web browser
-                    cookie[idx]['domain'] = 'kite.zerodha.com'
-                if each_dict['name'] == 'enctoken':
-                    with open(self.tokpath, 'w+') as wr:
-                        wr.write(each_dict['value'])
-                    print("Enctoken Updated")
-
-            driver.quit()
-            return True
-
-        except Exception as e:
-            print("Error while logging in using Selenium. Error: "+str(e))
-            traceback.print_exc()
-            return False
 
     @pre
     def order_place(self, **kwargs: List[Dict]):
