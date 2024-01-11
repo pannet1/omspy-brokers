88ff4d5241b658a6ba50f0e54995d772910604c2
pannet1 <prog@ecomsense.in>
2024-01-11 18:32:38 +0530

    alice new api

--- omspy_brokers/alice_blue.py
+++ omspy_brokers/alice_blue.py
@@ -11,36 +11,40 @@ class AliceBlue(Broker):
     def __init__(self, user_id, api_key):
         self._user_id = user_id
         self._api_key = api_key
-        self.alice = Aliceblue(user_id=user_id, api_key=api_key)
+        self.broker = Aliceblue(user_id=user_id, api_key=api_key)
         super(AliceBlue, self).__init__()
 
-    def authenticate(self) -> str:
+    def authenticate(self) -> bool:
         """
         Authenticate the user
         """
-        return self.alice.get_session_id()
+        token = self.broker.get_session_id()
+        if token and len(token) > 0:
+            self.token = token
+            return True
+        return False
 
     @pre
     def order_place(self, **kwargs):
-        pass
+        return self.broker.place_order(**kwargs)
 
     def order_modify(self, **kwargs):
-        pass
+        return self.broker.modify_order(**kwargs)
 
     def order_cancel(self, order_id):
-        pass
+        return self.broker.cancel_order(order_id)
 
     @property
     @post
-    def orders(self) -> List[Dict]:
+    def orders(self):
         return self.alice.get_order_history("")
 
     @property
     @post
     def positions(self):
-        pass
+        return self.alice.get_daywise_positions()
 
     @property
     @post
     def trades(self):
-        pass
+        return self.broker.get_trade_book()
