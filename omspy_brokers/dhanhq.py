a39baf300b795cd6cae58421425059dd894dbe85
pannet1 <prog@ecomsense.in>
2024-02-08 00:00:17 +0530

    order place

--- omspy_brokers/dhanhq.py
+++ omspy_brokers/dhanhq.py
@@ -18,18 +18,60 @@ class Dhanhq(Broker):
         """
         Authenticate the user
         """
-        if session := self.broker.session:
+        dct_margin = self.broker.get_fund_limits()
+        if dct_margin and \
+                isinstance(dct_margin, dict) and \
+                dct_margin["status"] == "success":
             return True
-        else:
-            print(f"{session =}")
-            return False
+        return False
+
+    def get_exchange_segment(self, exchange):
+        dct = dict(
+            NSE=self.broker.NSE,
+            BSE=self.broker.BSE,
+            CUR=self.broker.CUR,
+            MCX=self.broker.MCX,
+            FNO=self.broker.FNO,
+            NSE_FNO=self.broker.NSE_FNO,
+            BSE_FNO=self.broker.BSE_FNO,
+            NFO=self.broker.NSE_FNO,
+            BFO=self.broker.BSE_FNO
+        )
+        return dct.get(exchange, self.broker.NSE_FNO)
+
+    def get_order_type(self, order_type):
+        dct = dict(
+            LMT=self.broker.LIMIT,
+            MKT=self.broker.MARKET,
+            SLM=self.broker.SLM,
+            SL=self.broker.SL
+        )
+        return dct.get(order_type, self.broker.MARKET)
+
+    def get_product_type(self, product_type):
+        dct = dict(
+            MIS=self.broker.INTRA,
+            NRML=self.broker.DAY,
+        )
+        return dct.get(product_type, self.broker.MARGIN)
 
     @ pre
     def order_place(self, **kwargs: List[Dict]):
         """
         Place an order
         """
-        pass
+        symbol = kwargs.pop("symbol")
+
+        args = dict(
+            exchange_segment=self.get_exchange_segment(symbol[0]),
+            security_id=str(symbol[1]),
+            transaction_type=self.broker.BUY if kwargs["side"].upper()[
+                0] == "B" else self.broker.SELL,
+            quantity=kwargs["quantity"],
+            order_type=self.get_order_type(kwargs["order_type"]),
+            product_type=self.get_product_type(kwargs["product"])
+        )
+        self.broker.place_order(**args)
 
     @ pre
     def order_modify(self, **kwargs: List[Dict]):
